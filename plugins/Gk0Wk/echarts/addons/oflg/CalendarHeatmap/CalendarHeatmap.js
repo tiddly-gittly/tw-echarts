881a73db63f23870920961cb691033e1f74e4d0c
oflg <oflg@foxmail.com>
2022-02-12 21:03:45 +0800

    fix bug & format code

--- plugins/Gk0Wk/echarts/addons/oflg/CalendarHeatmap/CalendarHeatmap.js
+++ plugins/Gk0Wk/echarts/addons/oflg/CalendarHeatmap/CalendarHeatmap.js
@@ -1,71 +1,67 @@
 /*\
 title: $:/plugins/Gk0Wk/echarts/addons/oflg/CalendarHeatmap/CalendarHeatmap.js
 type: application/javascript
-module-type: library
+module-type: echarts-component
 
 Calendar Heatmap for TiddlyWiki
 \*/
-var TIME_OF_DAY = 3600 * 24 * 1000;
-exports.shouldUpdate = function (_, changedTiddlers) {
-  var shouldRefresh = false;
-  $tw.utils.each(changedTiddlers, function (_, key) {
-    if (!$tw.wiki.isShadowTiddler(key)) {
-      shouldRefresh = true;
-      return false;
-    }
-  });
-  return shouldRefresh;
-};
-exports.onUpdate = function (echart) {
-  function getData() {
-    var year = new Date().getFullYear();
-    var date = echarts.number.parseDate(year + '-01-01');
-    var end = echarts.number.parseDate((year + 1) + '-01-01');
-    var data = [];
-    for (var time = date; time < end; time += TIME_OF_DAY) {
-      var twtime = echarts.format.formatTime('yyyy-MM-dd', time).replace(/-/g, "");
-      data.push([
-        echarts.format.formatTime('yyyy-MM-dd', time),
-        $tw.wiki.filterTiddlers('[all[]!is[shadow]sameday:created[' + twtime + ']][all[]!is[shadow]sameday:modified[' + twtime + ']]+[count[]]')
-      ]);
-    }
-    return data;
-  }
-  echart.setOption({
-    tooltip: {
-      position: 'top',
-      formatter: function (p) {
-        var format = echarts.format.formatTime('yyyy-MM-dd', p.data[0]);
-        return format + ': ' + p.data[1];
-      }
-    },
-    visualMap: {
-      min: 0,
-      max: 10,
-      calculable: true,
-      orient: 'vertical',
-      left: 0,
-      top: 10
-    },
-    calendar: [
-      {
-        left: 90,
-        cellSize: 'auto',
-        top: 20,
-        orient: 'horizontal',
-        range: new Date().getFullYear(),
-        dayLabel: {
-          margin: 5
+exports.onUpdate = function (myChart) {
+    function getData(year) {
+        year = new Date().getFullYear();
+
+        var startDate = +echarts.number.parseDate(year + '-01-01'),
+            endDate = +echarts.number.parseDate(+year + 1 + '-01-01');
+
+        var dayTime = 3600 * 24 * 1000;
+
+        var data = [];
+
+        for (var time = startDate; time < endDate; time += dayTime) {
+            var twtime = echarts.format.formatTime('yyyy-MM-dd', time).replace(/-/g, "");
+            data.push([
+                echarts.format.formatTime('yyyy-MM-dd', time),
+                $tw.wiki.filterTiddlers('[all[]!is[shadow]sameday:created[' + twtime + ']][all[]!is[shadow]sameday:modified[' + twtime + ']]+[count[]]')
+            ]);
         }
-      }
-    ],
-    series: [
-      {
-        type: 'heatmap',
-        coordinateSystem: 'calendar',
-        calendarIndex: 0,
-        data: getData()
-      }
-    ]
-  });
+        return data;
+    }
+    var option = {
+        tooltip: {
+            position: 'top',
+            formatter: function (p) {
+                var format = echarts.format.formatTime('yyyy-MM-dd', p.data[0]);
+                return format + ': ' + p.data[1];
+            }
+        },
+        visualMap: {
+            min: 0,
+            max: 10,
+            calculable: true,
+            orient: 'vertical',
+            left: 0,
+            top: 10
+        },
+        calendar: [
+            {
+                left: 90,
+                cellSize: 'auto',
+                top: 20,
+                orient: 'horizontal',
+                range: new Date().getFullYear(),
+                dayLabel: {
+                    margin: 5
+                }
+            }
+        ],
+        series: [
+            {
+                type: 'heatmap',
+                coordinateSystem: 'calendar',
+                calendarIndex: 0,
+                data: getData()
+            }
+        ]
+    };
+
+    option && myChart.setOption(option);
 };
--- plugins/Gk0Wk/echarts/addons/oflg/Clock/ClockExample.js
+++ plugins/Gk0Wk/echarts/addons/oflg/Clock/ClockExample.js
@@ -1,219 +1,219 @@
 /*\
 title: $:/plugins/Gk0Wk/echarts/addons/oflg/Clock/Clock.js
 type: application/javascript
-module-type: library
+module-type: echarts-component
 
 Clock for TiddlyWiki
 \*/
 exports.onUpdate = function (echart) {
 
-  var option = {
-    series: [
-      {
-        name: 'hour',
-        type: 'gauge',
-        startAngle: 90,
-        endAngle: -270,
-        min: 0,
-        max: 12,
-        splitNumber: 12,
-        clockwise: true,
-        axisLine: {
-          lineStyle: {
-            width: 15,
-            color: [[1, 'rgba(0,0,0,0.7)']],
-            shadowColor: 'rgba(0, 0, 0, 0.5)',
-            shadowBlur: 15
-          }
-        },
-        splitLine: {
-          lineStyle: {
-            shadowColor: 'rgba(0, 0, 0, 0.3)',
-            shadowBlur: 3,
-            shadowOffsetX: 1,
-            shadowOffsetY: 2
-          }
-        },
-        axisLabel: {
-          fontSize: 30,
-          distance: 15,
-          formatter: function (value) {
-            if (value === 0) {
-              return '';
+    var option = {
+        series: [
+            {
+                name: 'hour',
+                type: 'gauge',
+                startAngle: 90,
+                endAngle: -270,
+                min: 0,
+                max: 12,
+                splitNumber: 12,
+                clockwise: true,
+                axisLine: {
+                    lineStyle: {
+                        width: 15,
+                        color: [[1, 'rgba(0,0,0,0.7)']],
+                        shadowColor: 'rgba(0, 0, 0, 0.5)',
+                        shadowBlur: 15
+                    }
+                },
+                splitLine: {
+                    lineStyle: {
+                        shadowColor: 'rgba(0, 0, 0, 0.3)',
+                        shadowBlur: 3,
+                        shadowOffsetX: 1,
+                        shadowOffsetY: 2
+                    }
+                },
+                axisLabel: {
+                    fontSize: 30,
+                    distance: 15,
+                    formatter: function (value) {
+                        if (value === 0) {
+                            return '';
+                        }
+                        return value + '';
+                    }
+                },
+                pointer: {
+                    icon: 'path://M2.9,0.7L2.9,0.7c1.4,0,2.6,1.2,2.6,2.6v115c0,1.4-1.2,2.6-2.6,2.6l0,0c-1.4,0-2.6-1.2-2.6-2.6V3.3C0.3,1.9,1.4,0.7,2.9,0.7z',
+                    width: 12,
+                    length: '55%',
+                    offsetCenter: [0, '8%'],
+                    itemStyle: {
+                        color: '#C0911F',
+                        shadowColor: 'rgba(0, 0, 0, 0.3)',
+                        shadowBlur: 8,
+                        shadowOffsetX: 2,
+                        shadowOffsetY: 4
+                    }
+                },
+                detail: {
+                    show: false
+                },
+                title: {
+                    offsetCenter: [0, '30%']
+                },
+                data: [
+                    {
+                        value: 0
+                    }
+                ]
+            },
+            {
+                name: 'minute',
+                type: 'gauge',
+                startAngle: 90,
+                endAngle: -270,
+                min: 0,
+                max: 60,
+                clockwise: true,
+                axisLine: {
+                    show: false
+                },
+                splitLine: {
+                    show: false
+                },
+                axisTick: {
+                    show: false
+                },
+                axisLabel: {
+                    show: false
+                },
+                pointer: {
+                    icon: 'path://M2.9,0.7L2.9,0.7c1.4,0,2.6,1.2,2.6,2.6v115c0,1.4-1.2,2.6-2.6,2.6l0,0c-1.4,0-2.6-1.2-2.6-2.6V3.3C0.3,1.9,1.4,0.7,2.9,0.7z',
+                    width: 8,
+                    length: '70%',
+                    offsetCenter: [0, '8%'],
+                    itemStyle: {
+                        color: '#C0911F',
+                        shadowColor: 'rgba(0, 0, 0, 0.3)',
+                        shadowBlur: 8,
+                        shadowOffsetX: 2,
+                        shadowOffsetY: 4
+                    }
+                },
+                anchor: {
+                    show: true,
+                    size: 20,
+                    showAbove: false,
+                    itemStyle: {
+                        borderWidth: 15,
+                        borderColor: '#C0911F',
+                        shadowColor: 'rgba(0, 0, 0, 0.3)',
+                        shadowBlur: 8,
+                        shadowOffsetX: 2,
+                        shadowOffsetY: 4
+                    }
+                },
+                detail: {
+                    show: false
+                },
+                title: {
+                    offsetCenter: ['0%', '-40%']
+                },
+                data: [
+                    {
+                        value: 0
+                    }
+                ]
+            },
+            {
+                name: 'second',
+                type: 'gauge',
+                startAngle: 90,
+                endAngle: -270,
+                min: 0,
+                max: 60,
+                animationEasingUpdate: 'bounceOut',
+                clockwise: true,
+                axisLine: {
+                    show: false
+                },
+                splitLine: {
+                    show: false
+                },
+                axisTick: {
+                    show: false
+                },
+                axisLabel: {
+                    show: false
+                },
+                pointer: {
+                    icon: 'path://M2.9,0.7L2.9,0.7c1.4,0,2.6,1.2,2.6,2.6v115c0,1.4-1.2,2.6-2.6,2.6l0,0c-1.4,0-2.6-1.2-2.6-2.6V3.3C0.3,1.9,1.4,0.7,2.9,0.7z',
+                    width: 4,
+                    length: '85%',
+                    offsetCenter: [0, '8%'],
+                    itemStyle: {
+                        color: '#C0911F',
+                        shadowColor: 'rgba(0, 0, 0, 0.3)',
+                        shadowBlur: 8,
+                        shadowOffsetX: 2,
+                        shadowOffsetY: 4
+                    }
+                },
+                anchor: {
+                    show: true,
+                    size: 15,
+                    showAbove: true,
+                    itemStyle: {
+                        color: '#C0911F',
+                        shadowColor: 'rgba(0, 0, 0, 0.3)',
+                        shadowBlur: 8,
+                        shadowOffsetX: 2,
+                        shadowOffsetY: 4
+                    }
+                },
+                detail: {
+                    show: false
+                },
+                title: {
+                    offsetCenter: ['0%', '-40%']
+                },
+                data: [
+                    {
+                        value: 0
+                    }
+                ]
             }
-            return value + '';
-          }
-        },
-        pointer: {
-          icon: 'path://M2.9,0.7L2.9,0.7c1.4,0,2.6,1.2,2.6,2.6v115c0,1.4-1.2,2.6-2.6,2.6l0,0c-1.4,0-2.6-1.2-2.6-2.6V3.3C0.3,1.9,1.4,0.7,2.9,0.7z',
-          width: 12,
-          length: '55%',
-          offsetCenter: [0, '8%'],
-          itemStyle: {
-            color: '#C0911F',
-            shadowColor: 'rgba(0, 0, 0, 0.3)',
-            shadowBlur: 8,
-            shadowOffsetX: 2,
-            shadowOffsetY: 4
-          }
-        },
-        detail: {
-          show: false
-        },
-        title: {
-          offsetCenter: [0, '30%']
-        },
-        data: [
-          {
-            value: 0
-          }
         ]
-      },
-      {
-        name: 'minute',
-        type: 'gauge',
-        startAngle: 90,
-        endAngle: -270,
-        min: 0,
-        max: 60,
-        clockwise: true,
-        axisLine: {
-          show: false
-        },
-        splitLine: {
-          show: false
-        },
-        axisTick: {
-          show: false
-        },
-        axisLabel: {
-          show: false
-        },
-        pointer: {
-          icon: 'path://M2.9,0.7L2.9,0.7c1.4,0,2.6,1.2,2.6,2.6v115c0,1.4-1.2,2.6-2.6,2.6l0,0c-1.4,0-2.6-1.2-2.6-2.6V3.3C0.3,1.9,1.4,0.7,2.9,0.7z',
-          width: 8,
-          length: '70%',
-          offsetCenter: [0, '8%'],
-          itemStyle: {
-            color: '#C0911F',
-            shadowColor: 'rgba(0, 0, 0, 0.3)',
-            shadowBlur: 8,
-            shadowOffsetX: 2,
-            shadowOffsetY: 4
-          }
-        },
-        anchor: {
-          show: true,
-          size: 20,
-          showAbove: false,
-          itemStyle: {
-            borderWidth: 15,
-            borderColor: '#C0911F',
-            shadowColor: 'rgba(0, 0, 0, 0.3)',
-            shadowBlur: 8,
-            shadowOffsetX: 2,
-            shadowOffsetY: 4
-          }
-        },
-        detail: {
-          show: false
-        },
-        title: {
-          offsetCenter: ['0%', '-40%']
-        },
-        data: [
-          {
-            value: 0
-          }
-        ]
-      },
-      {
-        name: 'second',
-        type: 'gauge',
-        startAngle: 90,
-        endAngle: -270,
-        min: 0,
-        max: 60,
-        animationEasingUpdate: 'bounceOut',
-        clockwise: true,
-        axisLine: {
-          show: false
-        },
-        splitLine: {
-          show: false
-        },
-        axisTick: {
-          show: false
-        },
-        axisLabel: {
-          show: false
-        },
-        pointer: {
-          icon: 'path://M2.9,0.7L2.9,0.7c1.4,0,2.6,1.2,2.6,2.6v115c0,1.4-1.2,2.6-2.6,2.6l0,0c-1.4,0-2.6-1.2-2.6-2.6V3.3C0.3,1.9,1.4,0.7,2.9,0.7z',
-          width: 4,
-          length: '85%',
-          offsetCenter: [0, '8%'],
-          itemStyle: {
-            color: '#C0911F',
-            shadowColor: 'rgba(0, 0, 0, 0.3)',
-            shadowBlur: 8,
-            shadowOffsetX: 2,
-            shadowOffsetY: 4
-          }
-        },
-        anchor: {
-          show: true,
-          size: 15,
-          showAbove: true,
-          itemStyle: {
-            color: '#C0911F',
-            shadowColor: 'rgba(0, 0, 0, 0.3)',
-            shadowBlur: 8,
-            shadowOffsetX: 2,
-            shadowOffsetY: 4
-          }
-        },
-        detail: {
-          show: false
-        },
-        title: {
-          offsetCenter: ['0%', '-40%']
-        },
-        data: [
-          {
-            value: 0
-          }
-        ]
-      }
-    ]
-  };
+    };
 
-  setInterval(function () {
-    var date = new Date();
-    var second = date.getSeconds();
-    var minute = date.getMinutes() + second / 60;
-    var hour = (date.getHours() % 12) + minute / 60;
-    option.animationDurationUpdate = 300;
-    echart.setOption({
-      series: [
-        {
-          name: 'hour',
-          animation: hour !== 0,
-          data: [{ value: hour }]
-        },
-        {
-          name: 'minute',
-          animation: minute !== 0,
-          data: [{ value: minute }]
-        },
-        {
-          animation: second !== 0,
-          name: 'second',
-          data: [{ value: second }]
-        }
-      ]
-    });
-  }, 1000);
+    setInterval(function () {
+        var date = new Date();
+        var second = date.getSeconds();
+        var minute = date.getMinutes() + second / 60;
+        var hour = (date.getHours() % 12) + minute / 60;
+        option.animationDurationUpdate = 300;
+        echart.setOption({
+            series: [
+                {
+                    name: 'hour',
+                    animation: hour !== 0,
+                    data: [{ value: hour }]
+                },
+                {
+                    name: 'minute',
+                    animation: minute !== 0,
+                    data: [{ value: minute }]
+                },
+                {
+                    animation: second !== 0,
+                    name: 'second',
+                    data: [{ value: second }]
+                }
+            ]
+        });
+    }, 1000);
 
-  option && echart.setOption(option);
+    option && echart.setOption(option);
 };
