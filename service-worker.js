80832b4d9b916d0d7dbb955ca7a1292b5067dee2
linonetwo <linonetwo@users.noreply.github.com>
2022-05-08 04:37:56 +0000

    deploy: 27d4703f3c3f41c94ece80af599d198e16608cf7

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@ const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
 const { ExpirationPlugin } = workbox.expiration;
 const { precacheAndRoute, matchPrecache } = workbox.precaching;
 
-precacheAndRoute([{"revision":"2202d1024158299c249bb2eeb50dc494","url":"favicon.ico"},{"revision":"b7d4c01e2cc12c89f1be9b7db8fb37a1","url":"index.html"},{"revision":"7f3033f4d2051feb6d032ae010b484ba","url":"tiddlywikicore-5.2.2.js"}]);
+precacheAndRoute([{"revision":"2202d1024158299c249bb2eeb50dc494","url":"favicon.ico"},{"revision":"c77e492dcc4446394cb89db8e07fd716","url":"index.html"},{"revision":"7f3033f4d2051feb6d032ae010b484ba","url":"tiddlywikicore-5.2.2.js"}]);
 
 registerRoute(
   /\.css$/,
