0411e5096a9539591c111288cd32c9daa230b7de
Gk0Wk <nmg_wk@yeah.net>
2023-10-23 13:50:05 +0800

    fix

--- src/echarts/addons/Gk0Wk/Function/Function.ts
+++ src/echarts/addons/Gk0Wk/Function/Function.ts
@@ -30,26 +30,10 @@ const addon: IScriptAddon<void> = {
     // 采样
     const points = [];
     const step = (maxX - minX) / Math.min(resolution, 1e5);
-    let minY = Infinity;
-    let maxY = -Infinity;
     for (let x = minX; x <= maxX; x += step) {
       const y = func(x);
       points.push([x, y]);
-      if (!Number.isNaN(y) && Number.isFinite(y)) {
-        minY = Math.min(minY, y);
-        maxY = Math.max(maxY, y);
-      }
     }
-    if (!Number.isFinite(minY)) {
-      minY = 0;
-    }
-    if (!Number.isFinite(maxY)) {
-      maxY = 0;
-    }
-    minY -= Math.max(1e-8, maxY - minY) * 5e-3;
-    maxY += Math.max(1e-8, maxY - minY) * 5e-3;
-    minX -= Math.max(1e-8, maxX - minX) * 5e-3;
-    maxX += Math.max(1e-8, maxX - minX) * 5e-3;
     myChart.setOption({
       animation: false,
       grid: {
@@ -61,15 +45,11 @@ const addon: IScriptAddon<void> = {
       tooltip: { trigger: 'axis' },
       xAxis: {
         name: 'x',
-        min: minX,
-        max: maxX,
         minorTick: { show: true },
         minorSplitLine: { show: true },
       },
       yAxis: {
         name: addonAttributes.yaxis || 'f(x)',
-        min: minY,
-        max: maxY,
         minorTick: { show: true },
         minorSplitLine: { show: true },
       },
